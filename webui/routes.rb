693851275709f7b3ef3d0f774ffd6beb274e1e66
Andrej Zakharevich <andrej@zahar.ws>
2015-09-13 10:42:17 +0300

    Sample page to display users list added

---
+++ webui/routes.rb
@@ -0,0 +1,15 @@
+require 'sinatra'
+require '../lib/users'
+
+set :port, 8080
+
+get "/" do
+	#@title = "User list"
+	list = ""
+
+	get_live_users.each{|user|
+		list += "%p #{user[0]}\n"
+	}
+	haml list
+	#haml :index
+end
